b22d93f99246f8d67db43bab28f3e2cc38ad9c53
Tassos Koutsovassilis <t.koutsovassilis@gmail.com>
2023-10-13 18:10:40 +0300

    added plugin files

---
+++ cubicast.php
@@ -0,0 +1,384 @@
+<?php
+/**
+ * @package Cubicast
+ * @version 0.1.0
+ * Plugin Name: Cubicast
+ * Plugin URI: http://wordpress.org/plugins/cubicast/
+ * Description: Afforfable session recording
+ * Author: Cubicast
+ * Version: 0.1.0
+ * Author URI: https://www.cubicast.com
+ *
+ * Text Domain: cubicast
+ * Domain Path: /languages/
+*/
+
+// Make sure we don't expose any info if called directly
+if ( !function_exists( 'add_action' ) ) {
+	echo 'Hi there!  I\'m just a plugin, not much I can do when called directly.';
+	exit;
+}
+
+define( 'CUBICAST_API_HOST', 'https://api.cubicast.com' );
+define( 'CUBICAST_APP_HOST', 'https://app.cubicast.com' );
+
+add_action('admin_menu', 'cubicast_create_menu');
+
+function cubicast_create_menu() {
+  add_menu_page(
+    __( 'Cubicast', 'cubicast' ),
+    __( 'Cubicast', 'cubicast' ),
+    'administrator',
+    __FILE__,
+    'cubicast_plugin_settings_page',
+    plugin_dir_url( __FILE__ ) . 'assets/images/cubicast_icon_white.svg'
+  );
+  add_action( 'admin_init', 'register_cubicast_plugin_settings' );
+}
+
+function register_cubicast_plugin_settings() {
+  register_setting( 'cubicast-plugin-settings-group', 'cubicast_api_key' );
+  add_option( 'cubicast_user_data', array() );
+}
+
+function cubicast_sync_wordpress_user() {
+  $cubicast_user_data = get_option( 'cubicast_user_data' );
+  $sync_name = in_array( 'name', $cubicast_user_data );
+
+  if (is_user_logged_in()) {
+    $current_user = wp_get_current_user();
+  }
+
+  $user_props = array();
+
+  // sync Wordpress user data
+  if ( isset( $current_user ) ) {
+    $email = $current_user->user_email;
+
+    if ( !empty( $email ) ) {
+      $user_id = $email;
+      $user_props['email'] = $user_id;
+    }
+
+    if ( isset( $current_user->display_name ) && in_array( 'nickname', $cubicast_user_data ) ) {
+      $user_props['nickname'] = $current_user->display_name;
+    }
+  
+    if ( $sync_name ) {
+      $first_name = $current_user->first_name;
+      $last_name = $current_user->last_name;
+      $user_props['first_name'] = $first_name;
+      $user_props['last_name'] = $last_name;
+    }
+  }
+
+  // sync WooCommerce customer data
+  if ( class_exists('WooCommerce') && !is_admin() ) {
+    $customer = WC()->session->get('customer');
+    if ($customer != NULL) {
+      if ( !isset( $user_id ) && !empty($customer['email']) ) {
+        // user not logged in
+        $user_id = $customer['email'];
+        $user_props['email'] = $user_id;
+      }
+      if ($sync_name) {
+        if ( isset( $customer['first_name'] ) && !empty( $customer['first_name'] ) ) {
+          $user_props['first_name'] = $customer['first_name'];
+        }
+        if ( isset( $customer['last_name'] ) && !empty( $customer['last_name'] ) ) {
+          $user_props['last_name'] = $customer['last_name'];
+        }
+      }
+      if (
+        isset( $customer['phone'] )
+        && !empty( $customer['phone'] )
+        && in_array( 'phone', $cubicast_user_data )
+      ) {
+        $user_props['phone'] = $customer['phone'];
+      }
+      if (
+        isset( $customer['company'] )
+        && !empty( $customer['company'] )
+        && in_array( 'company', $cubicast_user_data )
+      ) {
+        $user_props['company'] = $customer['company'];
+      }
+      if ( in_array( 'address', $cubicast_user_data ) ) {
+        $address_lines = array();
+        if ( isset( $customer['address'] ) && !empty( $customer['address'] ) ) {
+          $address_lines[] = $customer['address'];
+        }
+        if ( isset( $customer['address_2'] ) && !empty( $customer['address_2'] ) ) {
+          $address_lines[] = $customer['address_2'];
+        }
+        // get city, postcode and country
+        $last_line = array();
+        $address_keys = array(
+          'city',
+          'postcode',
+          'country',
+        );
+        foreach ($address_keys as $key) {
+          if ( isset( $customer[$key] ) && !empty( $customer[$key] ) ) {
+            $last_line[] = $customer[$key];
+          }
+        }
+        if ( count( $last_line ) > 0 ) {
+          $address_lines[] = implode(', ', $last_line);
+        }
+        if ( count( $address_lines ) > 0 ) {
+          $user_props['address'] = implode("\n", $address_lines);
+        }
+      }
+    }
+  }
+
+  if ( isset($user_id) ) {
+    return "cubicast.identify("
+      . json_encode($user_id)
+      . ", "
+      . json_encode($user_props, JSON_FORCE_OBJECT)
+      . ");";
+  }
+  return '';
+}
+
+function cubicast_settings_updated__success() {
+  ?>
+  <div id="setting-error-settings_updated" class="notice notice-success is-dismissible"> 
+    <p>
+      <strong><?php
+        _e('Settings saved.', 'cubicast');
+      ?></strong>
+    </p>
+  </div>
+  <?php
+}
+
+function cubicast_incative__warning() {
+  ?>
+  <div id="setting-error-settings_updated" class="notice notice-warning is-dismissible"> 
+    <p>
+      <strong>
+        <?php
+          _e('Cubicast is currently inactive.', 'cubicast');
+          echo(' ');
+          _e('To activate Cubicast, you need a valid API key.', 'cubicast');
+        ?>
+      </strong>
+    </p>
+  </div>
+  <?php
+}
+
+function cubicast_notice__error($message) {
+  ?>
+  <div id="setting-error-settings_updated" class="notice notice-error is-dismissible"> 
+    <p>
+      <strong><?php
+        _e($message, 'cubicast');
+      ?></strong>
+    </p>
+  </div>
+  <?php
+}
+
+function cubicast_validate_api_key($cubicast_api_key) {
+  $response = wp_remote_get( CUBICAST_API_HOST . '/api/v1/widget/' . $cubicast_api_key . '/workspace' );
+  $status = wp_remote_retrieve_response_code( $response );
+  $workspace_id = NULL;
+  if ($status == 200) {
+    $body = wp_remote_retrieve_body( $response );
+    $json_response = json_decode($body);
+    $workspace_id = $json_response->workspaceId;
+  }
+  return $workspace_id;
+}
+
+function cubicast_get_workspace_url($workspace_id) {
+  return CUBICAST_APP_HOST . "/workspaces/$workspace_id/recordings";
+}
+
+function cubicast_plugin_settings_page() {
+  $key_updated = false;
+  if (
+    isset( $_POST['_wpnonce'] ) 
+    && wp_verify_nonce( $_POST['_wpnonce'], 'update_cubicast_settings' ) 
+  ) {
+    if ( isset( $_POST['cubicast_api_key'] ) ) {
+      $cubicast_api_key = sanitize_text_field($_POST['cubicast_api_key']);
+      $key_updated = true;
+    }
+    if ( isset( $_POST['cubicast_user_data'] ) ) {
+      $cubicast_user_data = array_map( 'sanitize_text_field', $_POST['cubicast_user_data'] );
+      $valid_keys = array( 'name', 'nickname', 'phone', 'address', 'company' );
+      $invalid_keys = array_diff( $cubicast_user_data, $valid_keys );
+      if ( $invalid_keys ) {
+        // remove invalid keys
+        $cubicast_user_data = array_diff( $cubicast_user_data, $invalid_keys );
+      }
+    } else {
+      $cubicast_user_data = array();
+    }
+    update_option('cubicast_user_data', $cubicast_user_data);
+  } else {
+    $cubicast_api_key = get_option( 'cubicast_api_key' );
+    $cubicast_user_data = get_option( 'cubicast_user_data' );
+  }
+
+  if ( !empty( $cubicast_api_key ) ) {
+    $workspace_id = cubicast_validate_api_key( $cubicast_api_key );
+  
+    if ( $key_updated ) {
+      if ( $workspace_id != NULL ) {
+        // valid key provided
+        add_action( 'user_admin_notices', 'cubicast_settings_updated__success' );
+      } else {
+        // invalid key
+       $cubicast_api_key = '';
+      }
+      update_option('cubicast_api_key', $cubicast_api_key);
+    }
+  
+    if ( !isset( $workspace_id ) ) {
+      $message = 'Invalid Cubicast API key.';
+      add_action(
+        'user_admin_notices',
+        function() use ( $message ) {
+          cubicast_notice__error( $message );
+        }
+      );
+    }
+  } else if ( $key_updated ) {
+    // cubicast key cleared
+    update_option('cubicast_api_key', $cubicast_api_key);
+    add_action( 'user_admin_notices', 'cubicast_settings_updated__success' );
+  }
+
+  if ( empty( $cubicast_api_key ) || !isset( $workspace_id ) ) {
+    add_action( 'user_admin_notices', 'cubicast_incative__warning' );
+  }
+  ?>
+  <div class="wrap">
+    <h1><?php
+      _e('Cubicast Settings', 'cubicast');
+    ?></h1>
+    <?php
+      do_action('user_admin_notices');
+    ?>
+    <form method="POST">
+      <?php
+        wp_nonce_field( 'update_cubicast_settings' );
+      ?>
+      <table class="form-table" role="presentation">
+        <tr>
+          <th scope="row">
+            <label for="cubicast_api_key"><?php
+              _e('Cubicast Workspace API Key', 'cubicast');
+            ?></label>
+          </th>
+          <td>
+            <input
+              name="cubicast_api_key"
+              type="text"
+              id="cubicast_api_key"
+              value="<?php echo $cubicast_api_key; ?>"
+              class="regular-text"
+              maxlength="32"
+            />
+            <?php
+            if ($workspace_id) {
+            ?>
+              <input
+                type="button"
+                class="button"
+                value="<?php _e('View Sessions', 'cubicast') ?>"
+                onclick="javascript:window.open('<?php echo cubicast_get_workspace_url( $workspace_id ); ?>', 'cubicast')"
+              />
+            <?php
+            }
+            ?>
+          </td>
+        </tr>
+        <tr>
+          <th scope="row">
+            <?php _e('Sync User Data with Cubicast', 'cubicast'); ?>
+          </th>
+          <td>
+            <fieldset>
+              <label>
+                <input type="checkbox" name="cubicast_user_data[]" value="name"
+                  <?php if ( in_array( 'name', $cubicast_user_data ) ) echo 'checked' ?>
+                />
+                <span>
+                  <?php _e('User First Name and Last Name', 'cubicast'); ?>
+                </span>
+              </label><br>
+              <label>
+                <input type="checkbox" name="cubicast_user_data[]" value="nickname"
+                  <?php if ( in_array( 'nickname', $cubicast_user_data ) ) echo 'checked' ?>
+                />
+                <span>
+                  <?php _e('User Nickname', 'cubicast'); ?>
+                </span>
+              </label><br>
+              <label>
+                <input type="checkbox" name="cubicast_user_data[]" value="phone"
+                  <?php if ( in_array( 'phone', $cubicast_user_data ) ) echo 'checked' ?>
+                />
+                <span>
+                  <?php _e('Customer Phone', 'cubicast'); ?> (WooCommerce)
+                </span>
+              </label><br>
+              <label>
+                <input type="checkbox" name="cubicast_user_data[]" value="company"
+                  <?php if ( in_array( 'company', $cubicast_user_data ) ) echo 'checked' ?>
+                />
+                <span>
+                  <?php _e('Customer Company', 'cubicast'); ?> (WooCommerce)
+                </span>
+              </label><br>
+              <label>
+                <input type="checkbox" name="cubicast_user_data[]" value="address"
+                  <?php if ( in_array( 'address', $cubicast_user_data ) ) echo 'checked' ?>
+                />
+                <span>
+                  <?php _e('Customer Address', 'cubicast'); ?> (WooCommerce)
+                </span>
+              </label>
+            </fieldset>
+          </td>
+        </tr>
+      </table>
+      <p class="submit">
+        <input
+          type="submit"
+          name="submit"
+          id="submit"
+          class="button button-primary"
+          value="<?php _e('Save Changes', 'cubicast') ?>"
+        />
+      </p>
+    </form>
+  </div>
+  <?php
+}
+
+add_action( 'wp_head', 'cubicast_hook_head', 1 );
+
+function cubicast_hook_head() {
+  $cubicast_api_key = get_option('cubicast_api_key');
+
+  if ( !isset( $cubicast_api_key ) || empty( $cubicast_api_key ) ) {
+    return;
+  }
+
+  $output="<link rel=\"preconnect\" href=\"https://api.cubicast.com\">
+  <link rel=\"preconnect\" href=\"https://static.cubicast.com\">
+  <script data-cfasync=\"false\">!function(){ var cw=window.cubicast=window.cubicast||{ loaded:false, invoked:false, methods:['identify','track','err','showSupportRequestDialog'], config:{}, callQueue:[], methodFactory:function(method){ return function(){ var args=Array.prototype.slice.call(arguments); args.unshift(method); cw.callQueue.push(args); return cw; } }, load:function(k){ if(cw.loaded) return; var d=document; var s=d.createElement('script'); s.async=!0; s.src='https://static.cubicast.com/js/widget/widget.js'; var poll=setInterval(function(){ var w = window.cubicast; if(w.error || w.log){ clearInterval(poll); if(window.console && console.error){ if(w.error){ console.error(w.error); }else if(w.log){ console.log(w.log); } } }else if(w.loaded){ clearInterval(poll); w._fq(cw.callQueue); } }, 500); d.querySelector('head').appendChild(s); cw.config={apiKey:k}; } }; if(cw.invoked){ if(window.console && console.error) { console.error('Cubicast widget snippet included twice.'); } return; } cw.invoked=true; for(var i=0; i<cw.methods.length; i+=1){ var key=cw.methods[i]; cw[key]=cw.methodFactory(key); } cw.load('$cubicast_api_key'); }();";
+
+  $output .= cubicast_sync_wordpress_user();
+  $output .= "</script>";
+  
+  echo $output;
+}
---
+++ readme.txt
@@ -0,0 +1,84 @@
+=== Cubicast ===
+Contributors: cubicast
+Donate link: https://www.cubicast.com
+Tags: recordings, customer support, performance, error log, debug tool
+Requires at least: 5.3
+Tested up to: 6.3
+Requires PHP: 7.0
+Stable tag: 0.1.0
+License: MIT
+License URI: https://opensource.org/licenses/MIT
+
+Use visual feedback to help and understand your users.
+See through the eyes of your users and identify issues. Discover who, when, and why.
+
+== Description ==
+
+Cubicast is an affordable session recording service with some unique features built-in. Increase productivity for support agents, testers, and web developers. Track JavaScript errors, rage clicks, server performance bottlenecks, pinpoint bugs, and show solutions. **To utilize Cubicast, you need an active subscription plan after your expired 15-day trial period**.
+
+### How is your website performing around the globe?
+Keep track of slow resources, including slow-loading pages, API endpoints, scripts, images, and CSS resources with customizable thresholds.
+
+### Media capturing
+Does your website make use of canvas elements or videos? Cubicast is your best bet if you need to capture these media elements.
+
+### Advanced search
+Quickly locate what matters. Full-text searching in workspaces, including sessions, visitors, and events. Example use cases:
+
+1. Get all JavaScript errors that occurred in the last two days. Further, if you type `not defined` in the search term, you will get all JavaScript errors related to undefined variables. See which of your users are affected.
+2. List all input events that the users typed a specific term or selected a particular value in form elements.
+3. Get all identified users from a specific country that visited your site the last week.
+4. Locate all of the recordings created last week that have issues in general, whether this is JavaScript errors or slow-loading resources.
+5. Get all clicks tracked on a specific page by including any terms of the page URL. For example, if you want to get all clicks on a webpage having a URL of `http://example.com/downloads`, select `Clicks` as the event type and type `downloads` in the search term.
+
+### Slack and Zendesk integrations
+Cubicast integrates with Slack. Get real-time notifications on your preferred Slack channel about any activity in a workspace.
+Accept customer support requests to your Zendesk account. See what your users witnessed and their issues **before** asking for support.
+
+### Post-processing
+Capture any website using our Chrome and Firefox browser extensions. Add bookmarks and sticky rich-text annotations. Embed the annotated recordings to your website. Export recordings as native video or animated GIFs using our Chrome extension. Create video walkthroughs with minimum effort.
+
+### Crystal clear pricing
+Pricing is adjusted based on features and usage (API requests). Whether an individual or an enterprise, there is a proper plan for you. All plans come with a full-featured 15-day trial. No credit card is required.
+
+== Screenshots ==
+
+1. Recording screen
+2. Annotate recordings using the built-in Composer app
+3. Visitor screen
+
+== FAQ ==
+
+= How do I install Cubicast on my website? =
+
+1. Upload the plugin files to the `/wp-content/plugins/cubicast` directory, or install the plugin directly through the WordPress plugins screen.
+2. Add your workspace's API key to the Cubicast admin menu option. Optionally, choose which user attributes the plugin will keep in sync with Cubicast.
+
+= What is Cubicast? =
+
+Cubicast is a session recording service that helps keep your website in good shape and your end-users happy.
+
+= Who should use Cubicast? =
+
+Cubicast is ideal for any WordPress site, including but not limited to agencies, businesses, bloggers, designers, and, in general, any website owner.
+
+= Do I need coding skills to use the Cubicast WordPress plugin? =
+
+Out of the box, no. By default, once you set up Cubicast, you are good to go. You can use our simple [developer APIs](https://www.cubicast.com/developers/) if you need extra functionality, such as tracking custom events or preventing sensitive data from reaching our servers.
+
+= What about GDPR? =
+
+Cubicast is GDPR-ready. Since visitors are identified by their email, you should add Cubicast to your list of sub-processors. If you receive an erasure request, remove the visitor and ensure all PII no longer exists on our side.
+
+= How do I upgrade to a paid plan? =
+
+Cubicast comes with a 15-day trial. After your trial has expired, you can upgrade to a paid plan. See which subscription best suits you at our [pricing webpage](https://www.cubicast.com/pricing/).
+
+= Do you have another question? =
+
+[Contact us](https://www.cubicast.com/contact/), and we will do our best to address it.
+
+== Changelog ==
+
+= 0.1.0 =
+* First release
